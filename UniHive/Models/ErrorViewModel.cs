69dcc4ea5ad348dec5cef0c47f5e6fd0321c2300
Armend24 <armendlearns@gmail.com>
2025-05-31 15:41:05 +0200

    The SETUP

---
+++ UniHive/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniHive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
